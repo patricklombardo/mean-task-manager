b4f8f72ad7e464d1d2eac18a45e898b56c51efcd
Patrick Lombardo <patrickjohnlombardo@gmail.com>
2020-04-10 01:02:31 -0400

    add logic to mark tasks as complete

--- api/db/models/task.model.js
+++ api/db/models/task.model.js
@@ -10,6 +10,10 @@ const TaskSchema = new mongoose.Schema({
     type: mongoose.Types.ObjectId,
     required: true,
   },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 const Task = mongoose.model("Task", TaskSchema);
