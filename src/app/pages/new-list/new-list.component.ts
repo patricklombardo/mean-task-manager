e2545a6269cc5fdef2432e10f32c7b025d09d7c5
Patrick Lombardo <patrickjohnlombardo@gmail.com>
2020-04-10 00:11:15 -0400

    build out add new task functionality

--- src/app/pages/new-list/new-list.component.ts
+++ src/app/pages/new-list/new-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TaskService } from 'src/app/task.service';
+import { Router } from '@angular/router';
+import { List } from 'src/app/models/list.model';
 
 @Component({
   selector: 'app-new-list',
@@ -7,16 +9,16 @@ import { TaskService } from 'src/app/task.service';
   styleUrls: ['./new-list.component.scss'],
 })
 export class NewListComponent implements OnInit {
-  constructor(private taskService: TaskService) {}
+  constructor(private taskService: TaskService, private router: Router) {}
 
   ngOnInit(): void {}
 
   createList(title: string) {
     // Create the List
-    this.taskService.createList(title).subscribe((response: any) => {
+    this.taskService.createList(title).subscribe((response: List) => {
       console.log(response);
+      // Navigate /list/response._id
+      this.router.navigate(['/lists', response._id]);
     });
-
-    // Navigate /list/response._id
   }
 }
--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -7,16 +7,21 @@ import { WebRequestService } from './web-request.service';
 export class TaskService {
   constructor(private webReqService: WebRequestService) {}
 
-  createList(title: string) {
-    // Send an ajax request to create a list
-    return this.webReqService.post('lists', { title });
-  }
-
   getLists() {
     return this.webReqService.get('lists');
   }
 
   getTasks(listId: string) {
     return this.webReqService.get(`lists/${listId}/tasks`);
+  }
+
+  createList(title: string) {
+    // Send an ajax request to create a list
+    return this.webReqService.post('lists', { title });
+  }
+
+  createTask(title: string, listId: string) {
+    // Send an ajax request to create a task
+    return this.webReqService.post(`lists/${listId}/tasks`, { title });
   }
 }
